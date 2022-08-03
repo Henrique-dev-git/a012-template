6252e8b4514416cc0f3133b5f6a7997c894d4c36
Henrique-dev-git <carlos.henrique.psic@gmail.com>
2022-08-03 20:51:59 -0300

    completado

---
+++ script.js
@@ -0,0 +1,40 @@
+//parte 03
+// const bool1 = false;
+// const bool2 = true;
+// const bool3 = true;
+
+// // if (bool1) {
+// //     alert ('entrei no if sou true');
+// // } else {
+// //     alert ('entrei no else sou false');
+// // }
+//parte 02
+// if (bool1 === bool2){
+//     alert ("1 e 2 são iguais");
+// } else if (bool2 === bool3){
+//     alert ("2 e 3 são iguais");
+// } else if (bool1 === bool3){
+//     alert ("1 e 3 são iguais");
+// } else {
+//     alert ("Nenhuma das condições foi entendida!!!")
+// }
+//parte 03
+// let idade = prompt("qual sua idade?");
+
+// if(idade >= 18 && idade<60){
+//     alert("o é obrigatório")
+// }else{
+//    alert("o voto é facultativo")
+// }
+//fixação
+let media = 15
+
+if (media >= 5 ){
+    alert ('Aprovado');
+} else if (media>=3){
+    alert ('recuperação')
+} else if(media < 3) {
+    alert ('reprovado');
+} else {
+    alert ('dado inválido')
+}
